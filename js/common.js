b07a0958476387bcda017e8dd82c911cf0388867
AleksandrTs <a.carik@nastart-web.ru>
2025-03-05 17:01:35 +0500

    add gsap

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+"use strict";
 const buttonSelect = document.querySelector(".js-menu");
 const menuSelect = document.querySelector(".js-menu-drop");
 const bodyLock = document.querySelector("body");
@@ -24,36 +25,37 @@ const accordion = document.querySelectorAll(".accordion-modern");
 const accordion2 = document.querySelectorAll(".accordion-work");
 const accordion3 = document.querySelectorAll(".accordion-faq");
 
-accordion.forEach((element) => {
-  element.addEventListener("click", function () {
-    if (element.classList.contains("open")) {
-      element.classList.remove("open");
-    } else {
-      accordion.forEach((elem) => elem.classList.remove("open"));
-      element.classList.add("open");
-    }
-  });
-});
-accordion2.forEach((element) => {
-  element.addEventListener("click", function () {
-    if (element.classList.contains("open")) {
-      element.classList.remove("open");
-    } else {
-      accordion2.forEach((elem) => elem.classList.remove("open"));
-      element.classList.add("open");
-    }
-  });
-});
-accordion3.forEach((element) => {
-  element.addEventListener("click", function () {
-    if (element.classList.contains("open")) {
-      element.classList.remove("open");
-    } else {
-      accordion3.forEach((elem) => elem.classList.remove("open"));
-      element.classList.add("open");
-    }
-  });
-});
+//---временно не нужно
+// accordion.forEach((element) => {
+//   element.addEventListener("click", function () {
+//     if (element.classList.contains("open")) {
+//       element.classList.remove("open");
+//     } else {
+//       accordion.forEach((elem) => elem.classList.remove("open"));
+//       element.classList.add("open");
+//     }
+//   });
+// });
+// accordion2.forEach((element) => {
+//   element.addEventListener("click", function () {
+//     if (element.classList.contains("open")) {
+//       element.classList.remove("open");
+//     } else {
+//       accordion2.forEach((elem) => elem.classList.remove("open"));
+//       element.classList.add("open");
+//     }
+//   });
+// });
+// accordion3.forEach((element) => {
+//   element.addEventListener("click", function () {
+//     if (element.classList.contains("open")) {
+//       element.classList.remove("open");
+//     } else {
+//       accordion3.forEach((elem) => elem.classList.remove("open"));
+//       element.classList.add("open");
+//     }
+//   });
+// });
 
 window.addEventListener("DOMContentLoaded", function () {
   [].forEach.call(document.querySelectorAll(".phone-input"), function (input) {
@@ -144,115 +146,114 @@ document.addEventListener("click", function (event) {
   }
 });
 
-("use strict");
-function RandChar(prevChar) {
-  if (prevChar && prevChar.ttl) {
-    prevChar.ttl--;
-    return prevChar;
-  }
-  return {
-    char: CHARS[Math.floor(Math.random() * CHARS.length)],
-    ttl: Math.floor(Math.random() * CHAR_MAX_LIFE),
-  };
-}
-function generateDrop(prev) {
-  const chars = Array(Math.floor(Math.random() * (DROP_MAX_LENGTH - 5) + 15))
-    .fill(null)
-    .map(RandChar);
-  const fontSize = Math.floor(
-    Math.random() * (MAX_DROP_FONT_SIZE - MIN_DROP_FONT_SIZE) +
-      MIN_DROP_FONT_SIZE
-  );
-  const font = fontSize + "px monospace";
-  if (!prev) prev = {};
-  prev.x = Math.floor(Math.random() * canvas.width);
-  prev.y = Math.floor(
-    Math.random() * (canvas.height * 2) -
-      canvas.height * 2 -
-      chars.length * fontSize -
-      200
-  );
-  prev.chars = chars;
-  prev.font = font;
-  prev.timeToMove = 0;
-  return prev;
-}
-function scrambleDrop(drop) {
-  drop.chars = drop.chars.map(RandChar);
-  drop.chars.shift();
-  drop.chars.push(RandChar());
-  return drop;
-}
-const CHARS = "code7 7 code7 7".split("");
-const DROPS = 520;
-const DROP_MAX_LENGTH = 2;
-const CHAR_MAX_LIFE = 10;
-const MIN_DROP_FONT_SIZE = 10;
-const MAX_DROP_FONT_SIZE = 16;
-const DROP_SPEED = 100;
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d", { alpha: true });
-ctx.textRendering = "optimizeSpeed";
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const drops = [];
-for (let row = 0; row < DROPS; row++) {
-  drops.push(generateDrop());
-}
-let prevTime = 0;
-function drawText(text, x, y, fillStyle, font, shadowBlur = 7) {
-  ctx.shadowColor = fillStyle;
-  ctx.shadowOffsetX = 100;
-  ctx.shadowOffsetY = 200;
-  ctx.shadowBlur = 10;
-  ctx.fillStyle = fillStyle;
-  ctx.font = font;
-  ctx.fillText(text, x, y);
-}
-function draw(timestamp) {
-  const passedTime = timestamp - prevTime;
-  prevTime = timestamp;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < drops.length; i++) {
-    const fontSize = parseInt(drops[i].font);
-    for (let k = 0; k < drops[i].chars.length; k++) {
-      let color, shadowBlur;
-      if (k === drops[i].chars.length - 1) {
-        const s = 250 * (k / DROP_MAX_LENGTH);
-        color = `rgb(${s},${s},${s})`;
-        shadowBlur = 5;
-      } else {
-        color = `rgb(12, ${2 * (k / DROP_MAX_LENGTH)}, 0)`;
-        shadowBlur = 10;
-      }
-      drawText(
-        drops[i].chars[k].char,
-        drops[i].x,
-        drops[i].y + k * fontSize,
-        color,
-        drops[i].font,
-        shadowBlur
-      );
-    }
-    drops[i].timeToMove--;
-    if (drops[i].timeToMove <= 0) {
-      scrambleDrop(drops[i]);
-      drops[i].y += fontSize;
-      drops[i].timeToMove = fontSize ** -6.5 / (DROP_SPEED * 10 ** -10);
-    }
-    if (drops[i].y >= canvas.height) drops[i] = generateDrop(drops[i]);
-  }
-  window.requestAnimationFrame(draw);
-}
-window.requestAnimationFrame(draw);
-window.addEventListener("resize", (event) => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
+//----canvas
+// function RandChar(prevChar) {
+//   if (prevChar && prevChar.ttl) {
+//     prevChar.ttl--;
+//     return prevChar;
+//   }
+//   return {
+//     char: CHARS[Math.floor(Math.random() * CHARS.length)],
+//     ttl: Math.floor(Math.random() * CHAR_MAX_LIFE),
+//   };
+// }
+// function generateDrop(prev) {
+//   const chars = Array(Math.floor(Math.random() * (DROP_MAX_LENGTH - 5) + 15))
+//     .fill(null)
+//     .map(RandChar);
+//   const fontSize = Math.floor(
+//     Math.random() * (MAX_DROP_FONT_SIZE - MIN_DROP_FONT_SIZE) +
+//       MIN_DROP_FONT_SIZE
+//   );
+//   const font = fontSize + "px monospace";
+//   if (!prev) prev = {};
+//   prev.x = Math.floor(Math.random() * canvas.width);
+//   prev.y = Math.floor(
+//     Math.random() * (canvas.height * 2) -
+//       canvas.height * 2 -
+//       chars.length * fontSize -
+//       200
+//   );
+//   prev.chars = chars;
+//   prev.font = font;
+//   prev.timeToMove = 0;
+//   return prev;
+// }
+// function scrambleDrop(drop) {
+//   drop.chars = drop.chars.map(RandChar);
+//   drop.chars.shift();
+//   drop.chars.push(RandChar());
+//   return drop;
+// }
+// const CHARS = "code7 7 code7 7".split("");
+// const DROPS = 520;
+// const DROP_MAX_LENGTH = 2;
+// const CHAR_MAX_LIFE = 10;
+// const MIN_DROP_FONT_SIZE = 10;
+// const MAX_DROP_FONT_SIZE = 16;
+// const DROP_SPEED = 100;
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d", { alpha: true });
+// ctx.textRendering = "optimizeSpeed";
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// const drops = [];
+// for (let row = 0; row < DROPS; row++) {
+//   drops.push(generateDrop());
+// }
+// let prevTime = 0;
+// function drawText(text, x, y, fillStyle, font, shadowBlur = 7) {
+//   ctx.shadowColor = fillStyle;
+//   ctx.shadowOffsetX = 100;
+//   ctx.shadowOffsetY = 200;
+//   ctx.shadowBlur = 10;
+//   ctx.fillStyle = fillStyle;
+//   ctx.font = font;
+//   ctx.fillText(text, x, y);
+// }
+// function draw(timestamp) {
+//   const passedTime = timestamp - prevTime;
+//   prevTime = timestamp;
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   for (let i = 0; i < drops.length; i++) {
+//     const fontSize = parseInt(drops[i].font);
+//     for (let k = 0; k < drops[i].chars.length; k++) {
+//       let color, shadowBlur;
+//       if (k === drops[i].chars.length - 1) {
+//         const s = 250 * (k / DROP_MAX_LENGTH);
+//         color = `rgb(${s},${s},${s})`;
+//         shadowBlur = 5;
+//       } else {
+//         color = `rgb(12, ${2 * (k / DROP_MAX_LENGTH)}, 0)`;
+//         shadowBlur = 10;
+//       }
+//       drawText(
+//         drops[i].chars[k].char,
+//         drops[i].x,
+//         drops[i].y + k * fontSize,
+//         color,
+//         drops[i].font,
+//         shadowBlur
+//       );
+//     }
+//     drops[i].timeToMove--;
+//     if (drops[i].timeToMove <= 0) {
+//       scrambleDrop(drops[i]);
+//       drops[i].y += fontSize;
+//       drops[i].timeToMove = fontSize ** -6.5 / (DROP_SPEED * 10 ** -10);
+//     }
+//     if (drops[i].y >= canvas.height) drops[i] = generateDrop(drops[i]);
+//   }
+//   window.requestAnimationFrame(draw);
+// }
+// window.requestAnimationFrame(draw);
+// window.addEventListener("resize", (event) => {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// });
 
 //----animation
-
-const animItems = document.querySelectorAll("._anim__items");
+const animItems = document.querySelectorAll(".anim");
 
 if (animItems.length > 0) {
   window.addEventListener("scroll", animOnScroll);
@@ -273,10 +274,10 @@ if (animItems.length > 0) {
         pageYOffset > animItemOffset - animItemPoint &&
         pageYOffset < animItemOffset + animItemHeight
       ) {
-        animItem.classList.add("_anim-active");
+        animItem.classList.add("anim-active");
       } else {
-        if (!animItem.classList.contains("_anim_stop")) {
-          animItem.classList.remove("_anim-active");
+        if (!animItem.classList.contains("anim_stop")) {
+          animItem.classList.remove("anim-active");
         }
       }
     }
@@ -292,3 +293,72 @@ if (animItems.length > 0) {
     animOnScroll();
   }, 300);
 }
+
+//----GSAP
+const tl = gsap.timeline();
+tl.fromTo(
+  ".mix-item--second",
+  {
+    x: "-100%",
+    y: "100%",
+  },
+  {
+    y: 0,
+  });
+tl.fromTo(
+  ".mix-item--third",
+  {
+    x: "-100%",
+  },
+  {
+    x: "-200%",
+  }
+);
+tl.fromTo(
+  ".mix-item--fourth",
+  {
+    opacity: 0,
+    x: "-400%",
+  },
+  {
+    opacity: 1,
+    x: "-300%",
+    y: 0,
+  }
+);
+tl.fromTo(
+  ".mix-item--fifth",
+  {
+    x: "-400%",
+    y: "-100%",
+  },
+  {
+    y: 0,
+    x: '-400%'
+  }
+);
+tl.fromTo(
+  ".mix-item--sixth",
+  {
+    x: "-500%",
+    y: "100%",
+  },
+  {
+    y: 0,
+    x: '-500%'
+  }
+);
+const main = document.querySelector(".mix__list");
+
+ScrollTrigger.create({
+  animation: tl,
+  trigger: ".mix__list-wrap",
+  start: "center center",
+  end: () => main.offsetWidth / 1,
+  //end: "top center",
+  markers: true,
+  scrub: 2,
+  duration: 0.1,
+  pin: true,
+});
+
